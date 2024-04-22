adc3318451249de655d48c96e3dfae46d6cd0086
SoY0ung <752143070@qq.com>
2024-04-22 22:46:01 +0800

    [gui]Implement all func

--- gui_main.py
+++ gui_main.py
@@ -4,8 +4,10 @@ import subprocess
 import matplotlib.pyplot as plt
 import sys
 
-from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QFileDialog, QDialog
-from ui import ui_app, ui_adminDiag
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QFileDialog, QDialog, QInputDialog, QLineEdit, QWidget, QVBoxLayout, QLabel
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+from ui import ui_app, ui_adminDiag, ui_faceManagerWin
 
 from faceutil import FaceUtil
 
@@ -37,6 +39,220 @@ class AdminDialog(QDialog):
             QMessageBox.warning(self, '错误', '密码错误，请重试！')
             self.ui.pwdEdit.clear()
 
+class ImageViewer(QWidget):
+    def __init__(self, image_path):
+        super().__init__()
+        self.setWindowTitle('人脸预览')
+        self.setGeometry(300, 300, 350, 350) 
+
+        self.imageLabel = QLabel(self)
+        self.imageLabel.setAlignment(Qt.AlignCenter)
+
+        # 载入图片
+        pixmap = QPixmap(image_path)
+        self.imageLabel.setPixmap(pixmap.scaled(300, 300, Qt.KeepAspectRatio, Qt.SmoothTransformation))
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.imageLabel)
+        self.setLayout(layout)
+
+class FaceManagerWindow(QMainWindow):
+
+    def __init__(self, faceutil, logFunc):
+        super().__init__()
+        self.imageViewer = None 
+        self.faceutil = faceutil
+        self.logFunc = logFunc
+        self.ui = ui_faceManagerWin.Ui_MainWindow()  # 初始化界面
+        self.ui.setupUi(self)
+        self.logFunc('Opening Manager Window...')
+        self.load_face_data()
+
+        # 连接信号和槽
+        self.ui.refreshBtn.clicked.connect(self.load_face_data)
+        self.ui.cancelBtn.clicked.connect(self.close)
+        self.ui.camAddBtn.clicked.connect(self.camAdd)
+        self.ui.fileAddBtn.clicked.connect(self.fileAdd)
+        self.ui.camUpdBtn.clicked.connect(self.camUpd)
+        self.ui.fileUpdBtn.clicked.connect(self.fileUpd)
+        self.ui.delBtn.clicked.connect(self.delFace)
+
+        self.ui.faceDataList.itemSelectionChanged.connect(self.on_selection_changed)
+        self.ui.faceDataList.doubleClicked.connect(self.previewPic)
+
+    def load_face_data(self):
+        face_list = self.faceutil.list_face()
+        self.ui.faceDataList.clear()
+        for face_info in face_list:
+            self.ui.faceDataList.addItem(face_info) 
+
+    def camAdd(self):
+        QMessageBox.information(self, "提示",
+                                 "即将打开相机，取景框左上角倒计时结束后会自动拍照",
+                                 QMessageBox.Ok)
+        self.logFunc('Opening Camera...')
+        camface = faceutil.get_cam_faces()
+        if len(camface) > 0:
+            camface = camface[0]
+        else:
+            print("未检测到人脸!")
+            self.writeLog('No face detected!')
+            QMessageBox.warning(self, "警告",
+                                "未检测到人脸",
+                                QMessageBox.Ok)
+            return
+        if camface is not None:
+            text, okPressed = QInputDialog.getText(self, "添加人脸","请输入人脸名称", QLineEdit.Normal, "")
+            if okPressed and text != '':
+                if faceutil.add_face(camface, text):
+                    print('成功添加人脸：'+text)
+                    self.logFunc('Added face: '+text)
+                    self.load_face_data()
+                    QMessageBox.information(self, "人脸管理",
+                                 "人脸添加成功",
+                                 QMessageBox.Ok)
+                else:
+                    print('人脸名称已存在！')
+                    QMessageBox.warning(self, "人脸管理",
+                                 "人脸名称已存在",
+                                 QMessageBox.Ok)
+                    
+    def fileAdd(self):
+        options = QFileDialog.Options()
+        path, _ = QFileDialog.getOpenFileName(self, "选择图片", "",
+                                                  "JPEG Files (*.jpg);;All Files (*)", options=options)
+        if path:
+            self.logFunc(f"Image Path: {path}")
+            img = Image.open(path)
+            img = faceutil.get_img_faces(img)
+            if len(img) > 0:
+                img = img[0]
+            else:
+                print("未检测到人脸！")
+                self.logFunc('No face detected!')
+                QMessageBox.warning(self, "警告",
+                                "未检测到人脸",
+                                QMessageBox.Ok)
+                return
+        else:
+            return
+        if img is not None:
+            text, okPressed = QInputDialog.getText(self, "添加人脸","请输入人脸名称", QLineEdit.Normal, "")
+            if okPressed and text != '':
+                if faceutil.add_face(img, text):
+                    print('成功添加人脸：'+text)
+                    self.logFunc('Added face: '+text)
+                    self.load_face_data()
+                    QMessageBox.information(self, "人脸管理",
+                                 "人脸添加成功",
+                                 QMessageBox.Ok)
+                else:
+                    print('人脸名称已存在！')
+                    QMessageBox.warning(self, "人脸管理",
+                                 "人脸名称已存在",
+                                 QMessageBox.Ok)
+            
+    def previewPic(self):
+        selected_item = self.ui.faceDataList.selectedItems()[0].text()
+        image_path = database_path+selected_item+'.jpg'
+        self.image_viewer = ImageViewer(image_path=image_path)
+        self.image_viewer.show()
+
+    def camUpd(self):
+        selName = self.ui.faceDataList.selectedItems()[0].text()
+        if QMessageBox.information(self, "提示",
+                                 "即将打开相机，取景框左上角倒计时结束后会自动拍照",
+                                 QMessageBox.Ok|QMessageBox.Cancel) == QMessageBox.Cancel:
+            return
+        self.logFunc('Opening Camera...')
+        camface = faceutil.get_cam_faces()
+        if len(camface) > 0:
+            camface = camface[0]
+        else:
+            print("未检测到人脸!")
+            self.logFunc('No face detected!')
+            QMessageBox.warning(self, "警告",
+                                "未检测到人脸",
+                                QMessageBox.Ok)
+            return
+        if camface is not None:
+            if faceutil.update_face(camface, selName):
+                    print('成功更新人脸：'+selName)
+                    self.logFunc('Updated face: '+selName)
+                    QMessageBox.information(self, "人脸管理",
+                                 "人脸更新成功",
+                                 QMessageBox.Ok)
+
+    def fileUpd(self):
+        selName = self.ui.faceDataList.selectedItems()[0].text()
+        options = QFileDialog.Options()
+        path, _ = QFileDialog.getOpenFileName(self, "选择图片", "",
+                                                  "JPEG Files (*.jpg);;All Files (*)", options=options)
+        if path:
+            self.logFunc(f"Image Path: {path}")
+            img = Image.open(path)
+            img = faceutil.get_img_faces(img)
+            if len(img) > 0:
+                img = img[0]
+            else:
+                print("未检测到人脸！")
+                self.logFunc('No face detected!')
+                QMessageBox.warning(self, "警告",
+                                "未检测到人脸",
+                                QMessageBox.Ok)
+                return
+        else:
+            return
+        if img is not None:
+            if faceutil.update_face(img, selName):
+                    print('成功更新人脸：'+selName)
+                    self.logFunc('Updated face: '+selName)
+                    QMessageBox.information(self, "人脸管理",
+                                 "人脸更新成功",
+                                 QMessageBox.Ok)
+            else:
+                print('人脸名称已存在！')
+                QMessageBox.warning(self, "人脸管理",
+                                "人脸名称已存在",
+                                QMessageBox.Ok)
+                
+    def delFace(self):
+        selName = self.ui.faceDataList.selectedItems()[0].text()
+        if QMessageBox.question(self, "人脸删除",
+                                 f"确定要删除{selName}的人脸数据吗",
+                                 QMessageBox.Yes|QMessageBox.No) == QMessageBox.No:
+            return
+        if faceutil.remove_face(selName):
+            print('成功删除人脸：'+selName)
+            self.logFunc('Deleted face: '+selName)
+            self.load_face_data()
+            QMessageBox.information(self, "人脸管理",
+                                 "人脸删除成功",
+                                 QMessageBox.Ok)
+        else:
+            print('人脸名称不存在！')
+            QMessageBox.warning(self, "人脸管理",
+                                "人脸名称已存在",
+                                QMessageBox.Ok)
+        
+
+
+    def on_selection_changed(self):
+        selected_items = self.ui.faceDataList.selectedItems()
+        if selected_items:  # 检查列表是否不为空
+            # print("Selected Item:", selected_items[0].text())  # 输出选中的第一个项目
+            self.ui.camUpdBtn.setEnabled(True)
+            self.ui.fileUpdBtn.setEnabled(True)
+            self.ui.delBtn.setEnabled(True)
+        else:
+            self.ui.camUpdBtn.setEnabled(False)
+            self.ui.fileUpdBtn.setEnabled(False)
+            self.ui.delBtn.setEnabled(False)
+
+
+
+    
+
 
 class MainWindow(QMainWindow):
     admin_state = False
@@ -51,6 +267,7 @@ class MainWindow(QMainWindow):
         self.ui.camRecoBtn.clicked.connect(self.camReco)
         self.ui.fileRecoBtn.clicked.connect(self.fileReco)
         self.ui.switchUserBtn.clicked.connect(self.switchUser)
+        self.ui.openFaceMgrBtn.clicked.connect(self.open_face_manager)
 
     def writeLog(self, log):
         self.ui.debugOutputTextEdit.append(f"{log}")
@@ -136,7 +353,10 @@ class MainWindow(QMainWindow):
                 QMessageBox.information(self, "检测完毕",
                                 f"人脸姓名：{name}",
                                 QMessageBox.Ok)
-            
+
+    def open_face_manager(self):
+        self.faceManager = FaceManagerWindow(faceutil=faceutil, logFunc=self.writeLog) 
+        self.faceManager.show()
 
 if __name__ == '__main__':
     faceutil = FaceUtil(database_path=database_path)
